6540f913370f82da234f338e9767a4a417924b44
Debayan22 <debayansarkar786@gmail.com>
2022-08-26 13:55:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Dynamic field creation with javascript
+
+const addBtn = document.querySelector(".add");
+const input = document.querySelector(".inp-group");
+
+function removeInput(){
+    this.parentElement.remove();
+}
+
+function addInput(){
+    const name = document.createElement("input");
+    name.type = "text";
+    name.placeholder = "Enter Your Name";
+
+    const email = document.createElement("input");
+    email.type = "email";
+    email.placeholder="Enter Your Surname";
+
+    const btn = document.createElement("a");
+    btn.className = "delete";
+    btn.innerHTML="&times";
+
+    btn.addEventListener("click",removeInput);
+
+    const flex = document.createElement("div");
+    flex.className="flex";
+
+    input.appendChild(flex);
+    flex.appendChild(name);
+    flex.appendChild(email);
+    flex.appendChild(btn);
+}
+
+addBtn.addEventListener("click",addInput)
